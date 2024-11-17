6a1f0b20e038323bda72ab55c4768feeadf397f1
Sergio Vavassori <vavassori@gmail.com>
2024-11-17 14:53:10 +0100

    New URL for ISCIII report files

--- download.sh
+++ download.sh
@@ -3,17 +3,17 @@
 # download updated files
 base_url="https://www.sanidad.gob.es/areas/alertasEmergenciasSanitarias/alertasActuales/alertaMonkeypox"
 vaccines_url="https://www.sanidad.gob.es/areas/promocionPrevencion/vacunaciones/MonkeyPox"
-isciii_url="https://www.isciii.es/QueHacemos/Servicios/VigilanciaSaludPublicaRENAVE/EnfermedadesTransmisibles/Paginas/Resultados_Vigilancia_Viruela-del-mono.aspx"
+isciii_url="https://cne.isciii.es/es/web/cne/servicios/enfermedades-transmisibles/enfermedades-a-z/viruela-monos-monkeypox/resultados-vigilancia"
 
 links_pdfs=$(wget --no-verbose --output-document=- "${base_url}/home.htm" | grep --only-matching '"docs/[^"]\+\.pdf"' | tr -d '"')
 guides_pdfs=($(wget --no-verbose --output-document=- "${base_url}/guiaDeManejo.htm" | grep --only-matching '"docs/[^"]\+\.pdf"' | tr -d '"'))
 vaccines_pdfs=($(wget --no-verbose --output-document=- "${vaccines_url}/home.htm" | grep --only-matching 'href="[^"]\+.pdf"' | sed 's/href=//g' | tr -d '"'))
 
-opts="--no-verbose --timestamping --directory-prefix="
+opts="--no-verbose --timestamping --content-disposition --directory-prefix="
 
-wget ${opts}documentos/evaluación-rápida-riesgo "${base_url}"/$(echo "${links_pdfs}" | grep "_ERR_Monkeypox")
-wget ${opts}documentos/informes "${base_url}"/$(echo "${links_pdfs}" | grep "Informe_de_situacion")
-wget ${opts}documentos/protocolo "${base_url}"/$(echo "${links_pdfs}" | grep "ProtocoloMPX")
+wget ${opts}documentos/evaluación-rápida-riesgo "${base_url}/$(echo "${links_pdfs}" | grep "_ERR_Monkeypox")"
+wget ${opts}documentos/informes "${base_url}/$(echo "${links_pdfs}" | grep "Informe_de_situacion")"
+wget ${opts}documentos/protocolo "${base_url}/$(echo "${links_pdfs}" | grep "ProtocoloMPX")"
 
 # download remaining homepage's documents
 other_files=($(echo "${links_pdfs}" | grep --invert-match "ERR_Monkeypox\|Informe_de_situacion\|ProtocoloMPX"))
@@ -40,14 +40,15 @@ do
 done
 
 # downloads reports from ISCIII
-isciii_pdfs=$(wget --no-verbose --output-document=- "${isciii_url}" \
-	| sed 's/<a href=/\n/g' \
-	| grep '^"/QueHacemos/Servicios/VigilanciaSaludPublicaRENAVE/EnfermedadesTransmisibles/Documents/archivos%20A-Z/MPOX/SITUACION%20EPIDEMIOLOGICA' \
-	| awk -F '"' '{ print "https://www.isciii.es"$2 }' )
+wget --no-verbose --output-document=- "${isciii_url}" \
+  | awk '/situacion-epidemiologica-de-los-casos-de-mpox/ { print $2 }' \
+	| sed -e 's/href="//g' \
+	      -e 's/".*$//g' \
+	      -e 's|^/|https://cne.isciii.es/|g' \
+	| xargs wget ${opts}documentos/isciii
 
-echo -e "${isciii_pdfs}" | xargs wget ${opts}documentos/isciii
-
-# parse only the most-recent file (the one at the top)
-file_to_parse="$(realpath "$(echo "${isciii_pdfs}" | head --lines=1 | sed -e 's/%20/ /g' -e 's|.*/MPOX/|documentos/isciii/|g')")"
-
-./convert.sh "${file_to_parse}"
+# parse only the most-recent file
+# TODO look at file's timestamp?
+#file_to_parse="$(realpath "$(echo "${isciii_pdfs}" | head --lines=1 | sed -e 's/%20/ /g' -e 's|.*/MPOX/|documentos/isciii/|g')")"
+#
+#./convert.sh "${file_to_parse}"
